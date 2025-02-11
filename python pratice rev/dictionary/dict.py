523e6e3fac9cb0acbe52746e976976f6742ac392
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-11 18:32:53 +0530

    sn

--- python pratice rev/dictionary/dict.py
+++ python pratice rev/dictionary/dict.py
@@ -1,16 +1,16 @@
-x={"a":1,"b":2,"c":3,"d":"shyam","e":123.123,"f":24,"g":"nandu"}
+x={"a":1,"b":2,"c":3,"d":"shyam","e":123.123,"f":24,"g":"nandu",}
 # print(type(x))dict
 # print(x["g"])nandu
 # print(x["c"])3
 
 # print(x.get("a"))1 it gets key and gives output as value.
-x.update({"h":12344})
-print(x.update())
+# x.update({"h":12344})
+# print(x.update)
 # print(x.keys())
 # print(x.clear())
 # print(x.copy())
-# new_dict=x.fromkeys(["a","b","c"],0)
-# print(new_dict)
+new_dict=x.fromkeys(["a","b","c"],1)
+print(new_dict)
 # print(x.items())
 # y = x.pop("e")
 # print(y)
