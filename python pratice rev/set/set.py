ea2f74b5f6a75931e5ca3c2ed7f22bc9b54bebc2
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-12 17:27:50 +0530

    sn

--- python pratice rev/set/set.py
+++ python pratice rev/set/set.py
@@ -20,5 +20,18 @@ remove
 # print(a)
 
 '''
-
+union
+intersect
+difference
+issubset
+issuperset
 '''
+s1={123,456,789}
+s2={123,456,678,345,12345}
+#print(s1.union(s2))# prints all the values in one set.
+#print(s1.intersection(s2))#prints common in two sets.
+#print(s1.difference(s2))#789
+#print(s1.issubset(s2))false
+# print(s2.issubset(s1))false
+# print(s1.issuperset(s2))false
+print(s2.issuperset(s1))
