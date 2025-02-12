de34bb7947c854c58e22c517da1bc1ed64f37332
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-12 16:49:51 +0530

    sn

--- python pratice rev/set/set.py
+++ python pratice rev/set/set.py
@@ -1,3 +1,24 @@
 a={1,2,234,545,13434,4}
 # print(type(a))
-print()
+# print(a)
+'''
+add
+update
+pop
+remove
+'''
+#a.add("shyam")
+#print(a)#add in the random palces.
+
+# a.update({1,2,3,4,5})# updates the values with the values in a row.
+# print(a)
+
+# a.pop()# pop a random value
+# print(a)
+
+# a.remove(234)# removes the specified value.
+# print(a)
+
+'''
+
+'''
