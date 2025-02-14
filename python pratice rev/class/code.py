ba0d7569d2bce850658a7a6a1f2d2e27a05b908c
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-14 15:49:13 +0530

    sn

---
+++ python pratice rev/class/code.py
@@ -0,0 +1,56 @@
+'''
+class  : (it is ablueprint of an object.)
+object :
+'''
+'''
+class student():# creating a class.
+    name="shyam"
+    rollnumber=23
+    age=23
+    marks=8.7
+std1=student()# creating an object.
+print(std1.name)
+print(std1.rollnumber)
+print(std1.age)
+print(std1.marks)
+'''
+'''
+class computer():
+    a=5
+    print(a)#5
+'''
+
+# class computer():
+#     a=5
+#     def mine():
+#        print(a)   
+#     output()#NameError: name 'output' is not defined
+
+# class computer():
+#     a=5
+#     def mine(self):
+#        print(self.a)   
+# s=computer()
+# s.mine()
+
+# class shyam():
+#     a=143
+#     def nandu(self):
+#         print(self.a)
+# sana=shyam()
+# sana.nandu()
+     
+# class sana():# creating class
+#     nandu="sana is the daughter of nandu and shyam"
+#     def shyam(self):
+#         print(self.nandu)
+# daughter=sana() # object creation
+# daughter.shyam()
+
+# class pro():#class
+#     student=15
+#     def admin(self):
+#         print(self.student)
+# b=pro()# calling the class function
+# b.admin()#15 calling the object with class value.
+# print(b.student)# calling the object with class and object
