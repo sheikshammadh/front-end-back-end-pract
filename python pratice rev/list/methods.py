de9b2f38c66caaa20593155e1eb1a150525e13d4
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-14 16:03:25 +0530

    sn

--- python pratice rev/list/methods.py
+++ python pratice rev/list/methods.py
@@ -7,13 +7,18 @@ pop
 remove
 '''
 a=[124,234524,"sdg",25,"asfwg "]
-# a.append("shyam")
-# a.extend([124,1234,23,3,35])# extends the characters contineously.
-# print(a)# adds at last site.
-# print(a.count(25))# counts how many numbers are present in the given data.
-#print(a.pop(3))# it prints the indexing number.3 indexes the 25 so 25 is output
-#print(a.index(25))#it gives the indexing number.
-# looping:
-# for i in [124,234524,"sdg",25,"asfwg "]:
-#     print(i) fro loop.looping
-#a.insert(0,"shyam")# it adds the data at particular place. 0 is the insertion index, and what to add.
+a.append("shyam")
+a.extend([124,1234,23,3,35])# extends the characters contineously.
+print(a)# adds at last site.
+print(a.count(25))# counts how many numbers are present in the given data.
+print(a.pop(3))# it prints the indexing number.3 indexes the 25 so 25 is output
+print(a.index(25))#it gives the indexing number.
+looping:
+for i in [124,234524,"sdg",25,"asfwg "]:
+    print(i) fro loop.looping
+a.insert(0,"shyam")# it adds the data at particular place. 0 is the insertion index, and what to add.
+
+print(a[::1])# [124, 234524, 'sdg', 25, 'asfwg '] gives the list as it is.
+print(a[::-1])# ['asfwg ', 25, 'sdg', 234524, 124] reverse the list.
+print(a[:1])# [124] index value as 0.
+print(a[:-1])#[124, 234524, 'sdg', 25] prints all the list items except -1 index.
