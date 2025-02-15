604831bb6c622bfc45952d838909300b4b0ed2cb
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-15 16:45:48 +0530

    sn

--- python pratice rev/exceptional handling/code.py
+++ python pratice rev/exceptional handling/code.py
@@ -2,7 +2,7 @@
 when an error occurs, python will normally stop  adn generate an error meaasage
 these exception errors can be handled by the try statement
 wwe will handle by using exceptional handling
-'''
+''' 
 
 a=10
 # print(c)error.c is not defined
@@ -28,7 +28,7 @@ finally:
 # here no error so the else, and finally will handle. then it gives "a","no error","always".
 
 try:
-    print('s'+346)
+    print( 's'+346)
 except TabError:
     print("type error")
 except ValueError:
