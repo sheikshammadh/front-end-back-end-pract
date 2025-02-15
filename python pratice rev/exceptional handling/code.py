80127430c6f25d539212611f2c7b450d45c11756
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-15 16:22:22 +0530

    sn

---
+++ python pratice rev/exceptional handling/code.py
@@ -0,0 +1,37 @@
+'''
+when an error occurs, python will normally stop  adn generate an error meaasage
+these exception errors can be handled by the try statement
+wwe will handle by using exceptional handling
+'''
+
+a=10
+# print(c)error.c is not defined
+try:
+    print(c)# risky code(it has error so this is not executed)
+except:
+    print("error")#error
+else:
+    print("no error") #it has error so this is not executed
+finally:
+    print("always")# always
+
+
+a="shyam"
+try:
+    print("a")# risky code.
+except:
+    print("error")
+else:
+    print("no error") 
+finally:
+    print("always")
+# here no error so the else, and finally will handle. then it gives "a","no error","always".
+
+try:
+    print('s'+346)
+except TabError:
+    print("type error")
+except ValueError:
+    print("value error")
+
+# it will gives as TypeError
