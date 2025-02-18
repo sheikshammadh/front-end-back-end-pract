e360f02cef35e01c6dcf931c31693a0d207af5e3
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-18 17:57:10 +0530

    sn

---
+++ JS individual pract/operators/code.js
@@ -0,0 +1,65 @@
+/*
+var a=10;
+var b=20;
+console.log(typeof(a))//number
+console.log(typeof(b))//number
+console.log(a+b)//30
+console.log(a%b)//10
+*/
+/*
+var a=10;
+var b="20";
+console.log(typeof(a))//number
+console.log(typeof(b))//string
+console.log(a+b)//30  concatenation
+*/
+// ========================================================================================
+//ARITHMATIC OPERATORS
+/*
+var a=10;
+var b=2;
+console.log(a+b)//12
+console.log(a-b)//8
+console.log(a*b)//20
+console.log(a**b)//100
+console.log(a/b)//5
+console.log(a%b)//0
+*/ 
+// =========================================================================================
+//ASSIGNMENT OPERATOR
+/*
+var a=20
+var b=39
+b=b+1 // or b+=1 both are same.
+console.log(a)//20
+console.log(b)//40
+console.log(typeof(a))//number
+console.log(typeof(b))//number
+console.log(a+=b)//60 add
+console.log(a-=b)//20 sub
+console.log(a*=b)//800 mult
+console.log(a/=b)//20 
+console.log(a%=b)//20
+console.log(a**b)//1.099511627776e+52
+*/
+// ============================================================================================
+//COMPARISION OPERATOR
+/*
+let x=10;
+console.log(x==10)//true
+console.log(x==3)//false
+console.log(x=="10")//true
+console.log(x===10)//true
+console.log(x==="10")//false
+console.log(x!=10)//false
+console.log(x!==10)//false
+console.log(x!=="10")//true
+console.log(x!==8)//true
+console.log(x>10)//false
+console.log(x>10)//false
+*/
+// ==========================================================================================
+
+
+
+
---
+++ JS individual pract/var&identifiers/code.js
@@ -0,0 +1,9 @@
+var car="bmw";
+let price=100000;
+console.log(price+car)
+
+var a,b,c;
+a=34
+b=23
+c=12
+console.log(a,b,c)
