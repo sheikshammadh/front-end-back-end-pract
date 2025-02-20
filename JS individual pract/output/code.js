61075cb201587e788845550d0e9497164270507b
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-20 22:06:29 +0530

    sn

--- JS individual pract/output/code.js
+++ JS individual pract/output/code.js
@@ -2,3 +2,7 @@
 // b=40;
 // console.log(a+b);
 
+// Type conversions in JavaScript involve changing the datatype of a variable from one type to another. There are two types of type conversions: explicit and implicit.
+
+// Explicit Type Conversion
+// Explicit conversions are done manually by using built-in functions. Here are some common examples:
