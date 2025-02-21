68020ff7ef873de328e103e2ab2915a765851fce
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-21 12:23:45 +0530

    sn

--- JS individual pract/type conversions/code.js
+++ JS individual pract/type conversions/code.js
@@ -1,1 +1,27 @@
+//implicit to string: it concats the strong and value
+// var result='123'+7;
+// console.log(result)
+// =================================================================================
+// implicit to number:it takes both strings as numbers
+// var result='5'-'5';
+// console.log(result)//0
 
+// var result='5'-'10'
+// console.log(result)-5
+
+
+// var result='5'/'10'
+// console.log(result)0.5
+
+// var result='5'*'10'
+// console.log(result)50
+// =====================================================================================
+//implicit boolean to number: uses true=1 and false=0
+// var result='10'-true;
+// console.log(result)9 true value is 1 so it remobed 1 from 10
+
+// var result='5'*'5'+false;
+// console.log(result)25 false value is 0 so it is not added
+
+// var result='5'*'5'+true;
+// console.log(result)26 true value is 1 so ir added 1 to 25
