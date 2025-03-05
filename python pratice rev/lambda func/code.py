16826cc93152268b8a1c75964a8f5ce06243d228
sheikshammadh <sheikshammadh18@gmail.com>
2025-03-05 16:06:41 +0530

    sn

--- python pratice rev/lambda func/code.py
+++ python pratice rev/lambda func/code.py
@@ -4,6 +4,7 @@
 # print(double(5))
 
 #
-numbers=[1,2,3,4,5]
-double=list(map(lambda x:x*2,numbers))
-print(double)
+# numbers=[1,2,3,4,5]
+# double=list(map(lambda x:x*2,numbers))
+# print(double)
+
