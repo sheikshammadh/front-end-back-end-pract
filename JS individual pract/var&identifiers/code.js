31ec5ab8aeaa65793e57b9ec7cb8d014ef2480a2
sheikshammadh <sheikshammadh18@gmail.com>
2025-03-12 13:08:14 +0530

    sn

--- JS individual pract/var&identifiers/code.js
+++ JS individual pract/var&identifiers/code.js
@@ -1,9 +1,14 @@
-var car="bmw";
-let price=100000;
-console.log(price+car)
+// var car="bmw";
+// let price=100000;
+// console.log(price+car)
 
-var a,b,c;
-a=34
-b=23
-c=12
-console.log(a,b,c)
+// var a,b,c;
+// a=34
+// b=23
+// c=12
+// console.log(a,b,c)
+
+//Variables:
+var eid:1021;
+var ename:"shyam";
+var esal:
