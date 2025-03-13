538495b29fb5baea03c06a0cdb73ec25a4444d50
sheikshammadh <sheikshammadh18@gmail.com>
2025-03-13 10:52:55 +0530

    sn

--- python pratice rev/Oparators/code.py
+++ python pratice rev/Oparators/code.py
@@ -4,8 +4,25 @@
 # print(a is g)
 # print(a is s)
 
-f=[21,3,135,2,56,56,547,647]
-print(21 in (f))#true
-print(21 not in (f))#false
-print(26 in (f))#false
-print(26 not in (f))#true
+# f=[21,3,135,2,56,56,547,647]
+# print(21 in (f))#true
+# print(21 not in (f))#false
+# print(26 in (f))#false
+# print(26 not in (f))#true
+# ======================================================================
+
+# ============================================================================
+#verify elements in seq. by using membership op.
+unames=['shyam','nandu','vishnu','harika']#list
+enames=('amar','hemanth','venky')#tuple
+eid={101,102,103,104,105}#set
+emp={'eid':101,'ename':'shyam','esal':45000.45}#dict
+values=range(0,50,4)
+str='shyam'
+print('shyam' in unames)#True
+print('amar' in enames)#True
+print(101 in eid)#True
+print('esal' in emp)#True
+print(45000.45 in emp)#False
+print(25 in values)#False
+print('s' in str)#True
