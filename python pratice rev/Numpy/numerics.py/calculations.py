40b956d8be42fbf943ee5fbfa479526245bf3050
sheikshammadh <sheikshammadh18@gmail.com>
2025-03-15 15:10:23 +0530

    sn

--- python pratice rev/Numpy/numerics.py/calculations.py
+++ python pratice rev/Numpy/numerics.py/calculations.py
@@ -3,5 +3,13 @@ import numpy as sn
 o=sn.array([21,23,34,4])
 m=o+3
 print(m)#[24 26 37  7]
-n=o*6
-print(n)#[126 138 204  24]
+n=o-6
+print(n)#[15 17 28 -2]
+p=o*5
+print(p)#[105 115 170  20]
+q=o/3
+print(q)#[ 7.          7.66666667 11.33333333  1.33333333]
+r=o//2
+print(r)#[10 11 17  2]
+s=o%2
+print(s)#[1 1 0 0]
