93f76366676d85aa3dc04621f5e6e8adf23baf02
sheikshammadh <sheikshammadh18@gmail.com>
2025-03-18 17:27:02 +0530

    sn

--- python pratice rev/Pandas/indexing&slicing.py
+++ python pratice rev/Pandas/indexing&slicing.py
@@ -3,5 +3,7 @@ import pandas as pnd
 
 shyam=pnd.Series([1,2,3,4,5,6],['aa','bb','cc','dd','ee','ff'])
 nandu=pnd.Series([3,4,5,6,7,8],['AA','BB','CC','DD','EE','FF'])
-print('value of AA in shyam',shyam['aa'])#in shyam aa refers to 1,bb refers to 2.......etc.
+print('value of aa in shyam',shyam['aa'])#in shyam aa refers to 1,bb refers to 2.......etc.
 #value of AA in shyam 1
+print('value of DD in shyam',nandu['DD'])#in nandu AA refers to 3,BB refers to 4.......etc.
+#value of DD in shyam 6
