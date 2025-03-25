e867a02d226f421df5fefb0ae74ae33fe8738551
sheikshammadh <sheikshammadh18@gmail.com>
2025-03-25 11:55:59 +0530

    sn

---
+++ python pratice rev/all imports/importmath.py
@@ -0,0 +1,11 @@
+# import math
+# print (math.pi)#3.141592653589793
+# print (math.sqrt(64))#8.0
+# print (math.ceil(9.8))#10
+# print (math.floor(9.8))#9
+# ====================================================================================================================
+from math import pi,sqrt,ceil,floor
+print (pi)#3.141592653589793
+print (sqrt(64))#8.0
+print (ceil(9.8))#10
+print (floor(9.8))#9
