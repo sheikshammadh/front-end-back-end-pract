2cb98b610c3e520218b5dc56372f58b5fad8d7a1
sheikshammadh <sheikshammadh18@gmail.com>
2025-03-27 11:59:05 +0530

    sn

--- python pratice rev/Pandas/Locate_Row.py
+++ python pratice rev/Pandas/Locate_Row.py
@@ -17,7 +17,7 @@ Name: 1, dtype: int64
 '''
 #giving a each row name:
 df=pnd.DataFrame(data,index=['1st','2nd','3rd','4th','5th'])
-print(df)
+#print(df)
 '''
      shyam  nandu
 1st      1      5
@@ -26,3 +26,28 @@ print(df)
 4th      4      2
 5th      5      1
 '''
+#Locate named indexes:must use 'loc' attribute.
+#print(df.loc['3rd'])
+'''
+shyam    3
+nandu    3
+Name: 3rd, dtype: int64
+'''
+#Load files into Data Frame:
+df=pnd.read_csv('D:\Learn - Full Stack\practices,front end and back end\python pratice rev\Pandas\data.csv')
+#print(df)
+'''
+          Duration  Pulse  Maxpulse  Calories
+0    0          60    110       130     409.1
+1    1          60    117       145     479.0
+2    2          60    103       135     340.0
+3    3          45    109       175     282.4
+4    4          45    117       148     406.0
+5    164        60    105       140     290.8
+6    165        60    110       145     300.4
+7    166        60    115       145     310.2
+8    167        75    120       150     320.4
+9    168        75    125       150     330.4
+'''
+df=pnd.read_csv('D:\Learn - Full Stack\practices,front end and back end\python pratice rev\Pandas\data.csv')
+print(df)
---
+++ python pratice rev/Pandas/Read_CSV.py
@@ -0,0 +1,17 @@
+import pandas as pnd
+df=pnd.read_csv('D:\Learn - Full Stack\practices,front end and back end\python pratice rev\Pandas\data.csv')
+print(df.to_string())
+'''
+          Duration  Pulse  Maxpulse  Calories
+0    0          60    110       130     409.1
+1    1          60    117       145     479.0
+2    2          60    103       135     340.0
+3    3          45    109       175     282.4
+4    4          45    117       148     406.0
+5    164        60    105       140     290.8
+6    165        60    110       145     300.4
+7    166        60    115       145     310.2
+8    167        75    120       150     320.4
+9    168        75    125       150     330.4
+'''
+
