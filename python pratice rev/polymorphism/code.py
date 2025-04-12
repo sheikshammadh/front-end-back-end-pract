2d10a4c6afa152653650f16d5daf3ed0024181ae
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-12 22:38:39 +0530

    sn

--- python pratice rev/polymorphism/code.py
+++
@@ -1,9 +0,0 @@
-
-#adding the different forms in one unit.
-def add(shyam,nandu):
-    print(shyam+nandu)
-add(5,3)#8 (concat)
-add(124,145)#269 (concat)
-add("shyam","nandu")#shyamnandu (concat)
-add(234.441,214314.134325)#214548.57532499998 (concat)
-add([124,134],[124,14314])#[124, 134, 124, 14314] (concat)
