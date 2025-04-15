232522197af8e9a669b564aa2dfbd231e422ee9a
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-15 12:14:18 +0530

    sn

---
+++ python pratice rev/class/Constructor/one.py
@@ -0,0 +1,85 @@
+# class acc():
+#     pass
+
+#acc()
+
+# class acc():
+#     pass
+
+# acc(1,2,3,4).
+
+# class Acc():
+#     def __init__(self):
+#         print("constructor is a special method")
+#     def dep_amt(self):
+#         print("dep")
+#     @classmethod
+#     def update_amt(cls):
+#         print("upd")
+#     @staticmethod
+#     def sum_num(a,b):
+#         print("stc meth")
+# Acc()
+# Acc.dep_amt(10)
+# Acc.update_amt()
+# Acc.sum_num(1,2)
+
+
+
+# class Acc():
+#     def __init__(self,id,name,amount):
+#         self.id=id
+#         self.name=name
+#         self.acc_bal=amount
+#     def dep_amt(self,amount):
+#         self.acc_bal=self.acc_bal+amount
+#     # @classmethod
+#     # def update_amt(cls):
+#     #     pass
+
+#     # @staticmethod
+#     # def sum_num(a,b):
+#     #     pass
+# a1=Acc(101,"abc",45000)
+# a1.dep_amt(100)
+# print(a1.__dict__)#{'id': 101, 'name': 'abc', 'acc_bal': 45100}
+# a2=Acc(102,"cde",65000)
+# a2.dep_amt(200)
+# print(a2.__dict__){'id': 102, 'name': 'cde', 'acc_bal': 65100}
+# ========================================================================================================================================]
+# class Acc():
+#     def __init__(self,id,name,amount):
+#         self.id=id
+#         self.name=name
+#         self.acc_bal=amount
+#     def dep_amt(self,amount):
+#         self.acc_bal=self.acc_bal+amount
+#     def withdr_amt(self,amount):
+#         self.acc_bal=self.acc_bal-amount
+        
+# a1=Acc(101,"abc",45000)
+# a1.dep_amt(100)
+# a1.withdr_amt(10)
+# print(a1.__dict__)
+
+# a2=Acc(102,"cde",65000)
+# a2.dep_amt(200)
+# a2.withdr_amt(10)#{'id': 101, 'name': 'abc', 'acc_bal': 45090}
+# print(a2.__dict__)#{'id': 102, 'name': 'cde', 'acc_bal': 65190}
+
+
+class Acc():
+    a=10 # class variable
+    def __init__(self):
+        self.b=10 # instance variable
+        self.c=20 # instance variable
+    def open_acc(self):
+        pass
+a1=Acc()
+print(a1.__dict__)#{'b': 10, 'c': 20}
+print(Acc.__dict__)# adds some extra attributes internally
+'''{'__module__': '__main__', 'a': 10, '__init__': <function Acc.__init__ at 0x0000022555C8D3A0>,
+'open_acc': <function Acc.open_acc at 0x0000022555C8DF80>,
+'__dict__': <attribute '__dict__' of 'Acc' objects>,
+'__weakref__': <attribute '__weakref__' of 'Acc' objects>, '__doc__': None}
+'''
