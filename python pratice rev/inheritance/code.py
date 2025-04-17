d43310ac20924aaba45057837532d361ddeaebff
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-17 20:39:21 +0530

    sn

--- python pratice rev/inheritance/code.py
+++ python pratice rev/inheritance/code.py
@@ -13,92 +13,142 @@ hierarchical inhertance
 # one parent class and one child class
 
 
-class parnent():
-    def display(self):
-        print("parent class")
-class child(parnent):
-    def show(self):
-        print("child class")
-a=child()
-a.display()#parent class
-a.show()#child class
+# class parnent():
+#     def display(self):
+#         print("parent class")
+# class child(parnent):
+#     def show(self):
+#         print("child class")
+# a=child()
+# a.display()#parent class
+# a.show()#child class
 
 
-class shyam():
-    def me(self):
-        print("iam the father of sana")
-class sana(shyam):
-    def her(self):
-        print("iam the child")
-a=sana()#   defining the a with child class
-a.me()# ian=m the father of sana
-a.her()# iam the child
+# class shyam():
+#     def me(self):
+#         print("iam the father of sana")
+# class sana(shyam):
+#     def her(self):
+#         print("iam the child")
+# a=sana()#   defining the a with child class
+# a.me()# ian=m the father of sana
+# a.her()# iam the child
 
 # ======================================================================
 
 # multi level inheritance.
 # having more parent classes more than one.
 
-class grandfather():
-    def third(self):
-        print("iam the grandfather")
-class father(grandfather):
-    def second(self):
-        print("iam the father of child")
-class child(father):
-    def first(self):
-        print("iam the child")
-a=child()
-a.third()#grandfather
-a.second()# father of child
-a.first()#child
+# class grandfather():
+#     def third(self):
+#         print("iam the grandfather")
+# class father(grandfather):
+#     def second(self):
+#         print("iam the father of child")
+# class child(father):
+#     def first(self):
+#         print("iam the child")
+# a=child()
+# a.third()#grandfather
+# a.second()# father of child
+# a.first()#child
 
 # ==============================================================
 # multiple inheritance.
 # having two parent class and one child class.
 
-class father():
-    def parent1(self):
-        print("iam the father")
-class mother():
-    def parent2(self):
-        print("iam the mother")
-class child(father,mother):# accessing the both parent classes
-    def childself(self):
-        print("iam the child")
-a=child()#defining a as child()
-a.parent1()#iam the father.
-a.parent2()# iam the mother.
-a.childself()# iam the child.
+# class father():
+#     def parent1(self):
+#         print("iam the father")
+# class mother():
+#     def parent2(self):
+#         print("iam the mother")
+# class child(father,mother):# accessing the both parent classes
+#     def childself(self):
+#         print("iam the child")
+# a=child()#defining a as child()
+# a.parent1()#iam the father.
+# a.parent2()# iam the mother.
+# a.childself()# iam the child.
 
 # ================================================================
 #hierarchical inheritance
 
-class father():#we can use any class name
-    def parent(self):# we can use any method name
-        print("iam the father")#father class method 
-class child1(father):
-    def child_1(self):
-        print("iam the child 1")#child class method
-class child2(father):
-    def child_2(self):
-        print("iam the child 2")
-class child3(father):
-    def child_3(self):
-        print("iam the child 3")
-class child4(father):
-    def child_4(self):
-        print("iam the child 4")
-#invocations
-a1=child1()#child 1 class object creation
-a2=child2()#child 2 class object creation
-a3=child3()#child 3 class object creation
-a4=child4()#child 4 class object creation
-a.parent()#father class method
-a1.child_1()#child class method
-a2.child_2()#child 2
-a3.child_3()#child 3
-a4.child_4()#child 4
+# class father():#we can use any class name
+#     def parent(self):# we can use any method name
+#         print("iam the father")#father class method 
+# class child1(father):
+#     def child_1(self):
+#         print("iam the child 1")#child class method
+# class child2(father):
+#     def child_2(self):
+#         print("iam the child 2")
+# class child3(father):
+#     def child_3(self):
+#         print("iam the child 3")
+# class child4(father):
+#     def child_4(self):
+#         print("iam the child 4")
+# #invocations
+# a1=child1()#child 1 class object creation
+# a2=child2()#child 2 class object creation
+# a3=child3()#child 3 class object creation
+# a4=child4()#child 4 class object creation
+# a.parent()#father class method
+# a1.child_1()#child class method
+# a2.child_2()#child 2
+# a3.child_3()#child 3
+# a4.child_4()#child 4
 # type: ignore # ================================================================
 
- 
+# class parent1:
+#     pass
+# class parent2:
+#     pass
+# class parent3:
+#     pass
+# class child(parent1,parent2,parent3):
+#     pass
+
+# m1=parent1()
+# m2=parent2()
+# m2=parent3()
+
+
+
+# class Grandparent:
+#     pass
+# class parent1(Grandparent):
+#     pass
+# class parent2(Grandparent):
+#     pass
+# class parent3(Grandparent):
+#     pass
+# class child(parent1,parent2,parent3):
+#     pass
+    
+
+class Grandparent:
+    def m1(self):
+        print("A")
+class parent1(Grandparent):
+    def m2(self):
+        print("B")
+class parent2(Grandparent):
+    def m3(self):
+        print("C")
+class parent3(Grandparent):
+    def m4(self):
+        print("D")
+class child(parent1,parent2,parent3):
+    def m5(self):
+        print("E")
+    
+c1=child()
+c1.m1()
+c1.m2()
+c1.m3()
+c1.m4()
+
+
+
