467f23b1f016647b985c96e567147d961170de62
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-21 12:43:40 +0530

    sn

--- python pratice rev/API's/Requests get/insertintocsv.py
+++ python pratice rev/API's/Requests get/insertintocsv.py
@@ -1,11 +1,17 @@
-import csv,mysql.connector,json,requests
-dbcon=None
-#getting req.
-data=requests.get('https://jsonplaceholder.typicode.com/users')
+import json,mysql.connector,csv,requests 
+#Extract Data
+data = requests.get('https://jsonplaceholder.typicode.com/users')
 users=data.json()
-# new_users=[]
-fp=open('user.csv','w',newline='null')
-csvwriter=csv.write(fp)
-csv
+# load data
+fp = open('user.csv','w',newline="")
+csvwriter=csv.writer(fp)
+csvwriter.writerow(['id','name','loc','email'])
+
+for user in users:
+    csvwriter.writerow([user['id'],user['name'],user['address']['city'],user['email']])
+
+print("New CSV File Created and written successfully!")
+
+fp.close()
 
 
