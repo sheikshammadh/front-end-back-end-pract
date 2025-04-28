7c35adea2bdf21ea12b8f02c3166284063af8049
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-28 16:38:31 +0530

    sn

---
+++ python pratice rev/jumping statements/code.py
@@ -0,0 +1,43 @@
+'''
+pass
+break
+continue
+'''
+#==========================================
+# if True:
+#     #IndentationError: expected an indented block after 'if' statement on line 7
+
+# if True:
+#     pass # no error or no output
+# ==========================================
+# for i in "shyam":
+#     if i=="a":
+#         break
+#     print(i)output:s
+#                    h
+#                    y
+# breaks at "a" it terminates the entire code.
+# ===========================================
+
+# for i in "shyam":
+#     if i=="a":
+#         continue
+#     print(i)output:s
+#                    h
+#                    y
+#                    m
+# "a"letter skips and continues the remaining
+# ===========================================
+# for i in "shyam nandu":
+#     if i=="a":
+#         continue
+#     print(i)output:s
+#                    h
+#                    y
+#                    m
+
+#                    n
+#                    n
+#                    d
+#                    u
+# "a" letter skips at everywhere.
