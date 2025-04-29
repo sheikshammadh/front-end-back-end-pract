831123b2e5de6835f6d582d0b3572d8420b344f8
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-29 22:56:39 +0530

    sn

--- python pratice rev/Pandas/conditionalselect.py
+++ python pratice rev/Pandas/conditionalselect.py
@@ -1,12 +1,2 @@
 import numpy as np
 import pandas as pnd
-np.random.seed(101*10)
-matrix_data=np.random.rand(4,5)*5
-row_labels=['a','b','c','d']
-column_hd=['z','y','x','w','v']
-df=pnd.DataFrame(data=matrix_data,index=row_labels,columns=column_hd)
-print(df)
-print(df>=1)
-print(df<=1)
-print(df.loc["c","y"])
-# print(df.loc["c","y"])
