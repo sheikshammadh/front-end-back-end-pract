b1a8761bdc2489d67bd7623bacf22d0c71a932f1
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-29 22:50:30 +0530

    sn

--- python pratice rev/lambda func/code.py
+++ python pratice rev/lambda func/code.py
@@ -1,10 +1,10 @@
 #to double a number:
 
-# double=lambda x:x*2
-# print(double(5))
+double=lambda x:x*2
+print(double(5))
 
-#
-# numbers=[1,2,3,4,5]
-# double=list(map(lambda x:x*2,numbers))
-# print(double)
+
+numbers=[1,2,3,4,5]
+double=list(map(lambda x:x*2,numbers))
+print(double)
 
