a24fb9fcdd1393057405639975210943ae093286
sheikshammadh <sheikshammadh18@gmail.com>
2025-05-21 12:54:27 +0530

    sn

--- python pratice rev/Pandas/Subsetting_df.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pnd
-np.random.seed(101)
-matrix_data=np.random.rand(4,5)
-row_labels=['a','b','c','d']
-column_hd=['z','y','x','w','v']
-df=pnd.DataFrame(data=matrix_data,index=row_labels,columns=column_hd)
-print(df)
-print(df.loc['a','v'])
