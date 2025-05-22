1e42a7f2dded69be5f39f717b33d922b2f4be55a
sheikshammadh <sheikshammadh18@gmail.com>
2025-05-22 14:05:35 +0530

    sn

--- python pratice rev/file handling/code.py
+++ python pratice rev/file handling/code.py
@@ -5,8 +5,3 @@ with open('example.txt', 'w') as f:
 # Appending to a file (adds to the end)
 with open('example.txt', 'a') as f:
     f.write('This line is appended.\n')
-
-# Reading from a file
-with open('example.txt', 'r') as f:
-    content = f.read()
-    print(content)
